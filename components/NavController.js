2a028f2172244050b925557315e063683978b8df
DevRappers <devrappers@gmail.com>
2019-12-11 15:52:19 +0900

    AuthHome 기본페이지 만들기

--- components/NavController.js
+++ components/NavController.js
@@ -5,10 +5,9 @@ import AuthNavigation from '../navigation/AuthNavigation';
 import MainNavigation from '../navigation/MainNavigation';
 export default () => {
 	// 로그인상태와 로그인함수 로그아웃함수
-	// const isLoggedIn = useIsLoggedIn();
-	// const logIn = useLogIn();
-	// const logOut = useLogOut();
-	const isLoggedIn = true;
+	const isLoggedIn = useIsLoggedIn();
+	const logIn = useLogIn();
+	const logOut = useLogOut();
 
 	// 로그인 시 MainNavigation을 보여주고 그렇지 않을 경우 AuthNavigation을 보여줌
 	return <View style={{ flex: '1' }}>{isLoggedIn ? <MainNavigation /> : <AuthNavigation />}</View>;
--- screens/Auth/AuthHome.js
+++ screens/Auth/AuthHome.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import constants from '../../constants';
 
 const View = styled.View`
 	justify-content: center;
 	align-items: center;
 	flex: 1;
+	background-color: ${(props) => props.theme.mintColor};
 `;
 
-const Text = styled.Text``;
+const Image = styled.Image`
+	width: ${constants.width / 1.8};
+	margin-bottom: 0px;
+`;
+
+const Touchable = styled.TouchableOpacity``;
+
+const SignUpBtn = styled.View`
+	background-color: ${(props) => props.theme.blueColor};
+	padding: 10px;
+	margin: 0px 50px;
+	border-radius: 4px;
+	width: ${constants.width / 1.8};
+	margin-bottom: 25px;
+`;
+
+const SignUpBtnText = styled.Text`
+	color: white;
+	text-align: center;
+	font-weight: 600;
+	font-size: 18px;
+`;
+
+const LoginLink = styled.View``;
+
+const LoginLinkText = styled.Text`
+	color: white;
+	font-weight: 600;
+	font-size: 16px;
+`;
 
 export default ({ navigation }) => (
 	<View>
-		<Text>Auth Home</Text>
-		<TouchableOpacity onPress={() => navigation.navigate('Login')}>
-			<Text>Go to Login</Text>
-		</TouchableOpacity>
-		<TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-			<Text>Go to Signup</Text>
-		</TouchableOpacity>
+		<Image resizeMode={'contain'} source={require('../../assets/logo.png')} />
+		<Touchable onPress={() => navigation.navigate('Signup')}>
+			<SignUpBtn>
+				<SignUpBtnText>이메일로 계정 생성</SignUpBtnText>
+			</SignUpBtn>
+		</Touchable>
+		<Touchable onPress={() => navigation.navigate('Login')}>
+			<LoginLink>
+				<LoginLinkText>아이디가 있으신가요?</LoginLinkText>
+			</LoginLink>
+		</Touchable>
 	</View>
 );

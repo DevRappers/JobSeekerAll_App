319fa8ff86d6e3dcb43d946a914f9eeeab67f373
DevRappers <devrappers@gmail.com>
2019-12-13 11:13:30 +0900

    로그아웃 구현

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,37 +1,62 @@
 import React from 'react';
-import { Image, TouchableOpacity, Platform, View, Text } from 'react-native';
-import { ListItem, Avatar} from 'react-native-elements';
+import { TouchableOpacity, View, Text, Alert } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
 import styled from 'styled-components';
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles';
 import constants from '../constants';
 import PropTypes from 'prop-types';
+import { useLogOut } from '../AuthContext';
 
-const UserProfile = ({ avatar, username, email }) => (
-	<View>
-		<ListItem style={{marginTop:30,marginBottom:30}} leftAvatar={{ source: { uri: avatar } }} title={username} subtitle={email} bottomDivider/>
-        <Text style={{paddingLeft: 10, paddingBottom: 5}}>계정정보</Text>
-        <TouchableOpacity>
-        <ListItem title={'내정보 변경하기'} bottomDivider chevron />
-        </TouchableOpacity>
-        <TouchableOpacity>
-        <ListItem title={'나의 스터디'} bottomDivider chevron />
-        </TouchableOpacity>
-        <TouchableOpacity>
-        <ListItem title={'나의 취미모임'} bottomDivider chevron />
-        </TouchableOpacity>
-        <TouchableOpacity>
-        <ListItem title={'회원탈퇴'} bottomDivider chevron />
-        </TouchableOpacity>
-        <TouchableOpacity>
-        <ListItem title={'로그아웃'} bottomDivider chevron />
-        </TouchableOpacity>
-        <Text style={{marginTop:30, paddingLeft: 10, paddingBottom: 5}}>기타</Text>
-        <TouchableOpacity>
-        <ListItem title={'개발자 정보'} bottomDivider chevron />
-        </TouchableOpacity>
-	</View>
-);
+const UserProfile = ({ avatar, username, email }) => {
+	const logOut = useLogOut();
+	const logOutButton = () => {
+		Alert.alert(
+			'정말 로그아웃 하시겠습니까?',
+			'',
+			[
+				{
+					text: '취소',
+					onPress: () => console.log('로그아웃 취소'),
+					style: 'cancel'
+				},
+				{ text: '확인', onPress: () => logOut() }
+			],
+			{ cancelable: false }
+		);
+	};
+	return (
+		<View>
+			<ListItem
+				style={{ marginTop: 30, marginBottom: 30 }}
+				leftAvatar={{ source: { uri: avatar } }}
+				title={username}
+				subtitle={email}
+				bottomDivider
+			/>
+			<Text style={{ paddingLeft: 10, paddingBottom: 5 }}>계정정보</Text>
+			<TouchableOpacity>
+				<ListItem title={'내정보 변경하기'} bottomDivider chevron />
+			</TouchableOpacity>
+			<TouchableOpacity>
+				<ListItem title={'나의 스터디'} bottomDivider chevron />
+			</TouchableOpacity>
+			<TouchableOpacity>
+				<ListItem title={'나의 취미모임'} bottomDivider chevron />
+			</TouchableOpacity>
+			<TouchableOpacity>
+				<ListItem title={'회원탈퇴'} bottomDivider chevron />
+			</TouchableOpacity>
+			<TouchableOpacity onPress={logOutButton}>
+				<ListItem title={'로그아웃'} bottomDivider chevron />
+			</TouchableOpacity>
+			<Text style={{ marginTop: 30, paddingLeft: 10, paddingBottom: 5 }}>기타</Text>
+			<TouchableOpacity>
+				<ListItem title={'개발자 정보'} bottomDivider chevron />
+			</TouchableOpacity>
+		</View>
+	);
+};
 
 UserProfile.propTypes = {
 	id: PropTypes.string.isRequired,

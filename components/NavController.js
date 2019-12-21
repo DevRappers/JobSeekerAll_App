0dbad8e136374e267ced88ec4842baa5197efd8a
DevRappers <devrappers@gmail.com>
2019-12-21 15:35:33 +0900

    취준모아 설명 작성

--- components/NavController.js
+++ components/NavController.js
@@ -10,7 +10,7 @@ import MainNavigation from '../navigation/MainNavigation';
 
 export default () => {
 	// 컨텍스트함수를 사용
-	//const isLoggedIn = true
+	//const isLoggedIn = false;
 	const isLoggedIn = useIsLoggedIn();
 	return <View style={{ flex: '1' }}>{isLoggedIn ? <MainNavigation /> : <AuthNavigation />}</View>;
 };
